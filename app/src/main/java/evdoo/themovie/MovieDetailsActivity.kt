1a8b2bbdde987b22f16cece973acf2878ba1488c
evdoo <o.evdokima@gmail.com>
2021-04-11 23:42:06 +0300

    Сверстан экран MovieDetailsActivity. Создана MainActivity. Сделан переход из MainActivity в MovieDetailsActivity

---
+++ app/src/main/java/evdoo/themovie/MovieDetailsActivity.kt
@@ -0,0 +1,11 @@
+package evdoo.themovie
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MovieDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_details)
+    }
+}
