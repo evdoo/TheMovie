cdc44426f5118926522127179d31e4d9755419d4
evdoo <o.evdokima@gmail.com>
2022-09-08 23:54:11 +0300

    [ref] Перемещен и переназван MoviesDetailsFragment

--- app/src/main/java/evdoo/themovie/fragments/MoviesDetailsFragment.kt
+++ app/src/main/java/evdoo/themovie/fragments/MoviesDetailsFragment.kt
@@ -1,4 +1,4 @@
-package evdoo.themovie.activities
+package evdoo.themovie.fragments
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import evdoo.themovie.R
 
-class FragmentMoviesDetails: Fragment() {
+class MoviesDetailsFragment: Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
