ccd3be6511122a145144e6aa92a8a6a633af7f45
evdoo <o.evdokima@gmail.com>
2022-09-08 23:51:51 +0300

    [feat] Добавлена MainActivity

---
+++ app/src/main/java/evdoo/themovie/activities/MainActivity.kt
@@ -0,0 +1,13 @@
+package evdoo.themovie.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import evdoo.themovie.R
+
+class MainActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
